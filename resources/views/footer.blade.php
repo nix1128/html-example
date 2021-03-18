1d838fc5c5c73099ef970669ce7e0e9400351f53
nix nix <sabev.nikolai@gmail.com>
2021-03-18 18:58:55 +0200

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,92 @@
+<footer class="section-footer bg-dark white">
+
+
+    <div class="align-content-lg-center">
+
+        <div class="col-xl-12 text-center ">
+            <br>
+            <h4 class=" text-white">НИКОГА НЕ ПРОПУСКАЙТЕ ОФЕРТА!</h4>
+            <h6 class=" text-white">Абонирайте се за нашия бюлетин!</h6>
+
+
+
+            <input  type="text" placeholder="E-MAIL">
+
+<br>
+                <div class="row ">
+
+                    <div class="col text-center">
+                        <button class=" btn btn-secondary">Абонирай се!</button>
+                    </div>
+                </div>
+
+
+
+        </div>
+    </div>
+
+    <br>
+    <div class="container">
+        <section class="footer-top padding-top">
+            <div class="row">
+
+                <hr>
+                <aside class="col-sm-3 col-md-3 white">
+
+                    <ul class="list-unstyled text-white">
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+
+
+                    </ul>
+                </aside>
+             <aside class="col-sm-3  col-md-3 white">
+
+                    <ul class="list-unstyled">
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                    </ul>
+                </aside>
+                <aside class="col-sm-3  col-md-3 white">
+
+                    <ul class="list-unstyled">
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                    </ul>
+                </aside>
+
+                <aside class="col-sm-3  col-md-3 white">
+
+                    <ul class="list-unstyled">
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                        <li ><a href="#" class="text-white">Link</a></li>
+                    </ul>
+                </aside>
+
+
+
+            </div>
+
+            <br>
+        </section>
+
+
+    </div>
+
+</footer>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <style>
+
+        .carousel-control-prev{
+           color:black;
+
+        }
+    </style>
+</head>
+
+
+<div id="carouselExampleCaptions" class="carousel" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/images/girl-eichorn-outdoor.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p class="text-success">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="assets/images/girl-eichorn-outdoor.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+
+            <img src="assets/images/eichhorn_wooden_train.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+
+    </div>
+
+
+</div>
+
+
+
+
+
+
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only text-light bg-dark">Next</span>
+    </a>
+
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <button class="btn btn-warning">КАТЕГОРИИ</button>
+            </div>
+        </div>
+    </div>
+
+
+
+<hr>
+<div class="row col">
+
+    <div class=" col-md-3 col-xs-4 ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/350x300" alt="">
+        </a>
+    </div>
+    <div class=" col-md-3 col-xs-4 ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/350x300" alt="">
+        </a>
+    </div>
+    <div class=" col-md-3 col-xs-45 ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/350x300" alt="">
+        </a>
+    </div>
+
+    <div class=" col-md-3 col-xs-4 ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/350x300" alt="">
+        </a>
+    </div>
+
+</div>
+
+
+
+<hr>
+
+<div class=" d-flex justify-content-center">
+
+    <div class=" ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/650x300" alt="">
+        </a>
+    </div>
+    <div class=" col-md-6  ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/650x300" alt="">
+        </a>
+    </div>
+
+</div>
+
+<hr>
+
+
+
+<hr>
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <button class=" btn btn-warning">ИЗБРАНИ ПРОДУКТИ</button>
+        </div>
+    </div>
+</div>
+
+<hr>
+<div class="row col">
+
+    <div class=" col-md-3 col-xs-4 ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/350x300" alt="">
+        </a>
+    </div>
+    <div class=" col-md-3 col-xs-4 ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/350x300" alt="">
+        </a>
+    </div>
+    <div class=" col-md-3 col-xs-45 ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/350x300" alt="">
+        </a>
+    </div>
+
+    <div class=" col-md-3 col-xs-4 ">
+        <a class="thumbnail" href="#">
+            <img class="img-responsive" src="http://placehold.it/350x300" alt="">
+        </a>
+    </div>
+
+</div>
+
+<hr>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
